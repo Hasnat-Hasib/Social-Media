a78af1ca35fde2417c4584898902a41f8350ff4e
unknown <hasib8nh@gmail.com>
2023-05-05 12:04:52 +0600

    Left Sidebar

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="dark">
-    <h1 class="text-red-500 dark:text-dim-500">Social Media </h1>
+  <div :class="{'dark': darkMode}">
+    <div class="bg-white dark:bg-dim-900">
+    <div class="min-h-full ">
+      <div class="grid grid-cols-12 mx-auto sm:px-0 lg:max-w-7xl lg:px-7 lg:gap-0">
+        <!-- Left side bar-->
+        <div class=" md:block xs-col-span-1 xl:col-span-2">
+          <div class="sticky top-0">
+            <SidebarLeft/>
+
+          </div>
+
+        </div>
+        <!-- Main Content-->
+        <!-- Right side bar-->
+
+
+      </div>
+    </div>
+  </div>
   </div>
 </template>
+
+<script setup>
+  const darkMode = ref(false)
+</script>
