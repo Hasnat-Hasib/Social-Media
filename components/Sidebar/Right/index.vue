389ebbb94447447ae0635eb923aae4ad982f0250
unknown <hasib8nh@gmail.com>
2023-05-09 12:28:34 +0600

    What's happening

--- components/Sidebar/Right/index.vue
+++ components/Sidebar/Right/index.vue
@@ -1,12 +1,37 @@
 <template>
     <div class="flex flex-col">
         <!--Preview Card: What's happening-->
-        <SidebarRightPreviewCard title="what's happening"/>
+        <SidebarRightPreviewCard title="what's happening">
+            <SidebarRightPreviewCardItem v-for="whatsHappening in whatsHappeningItems">
+                <div>
+                    <h2 class="font-bold text-gray-800 text-md dark:text-white">{{ whatsHappening.title }}</h2>
+
+                    <p class="text-xs text-gray-400">
+                        {{ whatsHappening.count }}
+                    </p>
+                </div>
+            </SidebarRightPreviewCardItem>
+        </SidebarRightPreviewCard>
         <!--Preview Card: Who to follow-->
         <SidebarRightPreviewCard title="who to follow"/>
     </div>
 </template>
 <script setup>
+
+const whatsHappeningItems = ref([
+    {
+        title: 'SpaceX',
+        count: '18.8k Tweets'
+    },
+    {
+        title: '#CodingIsFun',
+        count: '8.8k Tweets'
+    },
+    {
+        title: '#artforall',
+        count: '1.8k Tweets'
+    }
+])
 
 </script>
 
