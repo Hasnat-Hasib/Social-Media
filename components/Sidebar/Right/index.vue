0b15981fc4107525ea7464deec9e67d4cd9abf5f
unknown <hasib8nh@gmail.com>
2023-05-09 16:46:06 +0600

    Right Sidebar complete

--- components/Sidebar/Right/index.vue
+++ components/Sidebar/Right/index.vue
@@ -13,7 +13,24 @@
             </SidebarRightPreviewCardItem>
         </SidebarRightPreviewCard>
         <!--Preview Card: Who to follow-->
-        <SidebarRightPreviewCard title="who to follow"/>
+        <SidebarRightPreviewCard title="Who to follow">
+            <SidebarRightPreviewCardItem v-for="whoToFollow in whoToFollowItems">
+                <div class="flex flex-row items-center justify-between p-2">
+                    <div class="flex flex-row">
+                        <img class="w-10 h-10 rounded-full" :src="whoToFollow.image" :alt="whoToFollow.name">
+
+                        <div class="flex flex-col ml-2">
+                            <h1 class="text-sm font-bold text-gray-900 dark:text-white">{{ whoToFollow.name }}</h1>
+                            <p class="text-xs text-gray-400">{{ whoToFollow.handle }}</p>
+                        </div>
+                    </div>
+                    <div class="flex h-full">
+                        <button
+                            class="px-4 py-2 text-xs font-bold text-white bg-black rounded-full dark:text-black dark:bg-white">Follow</button>
+                    </div>
+                </div>
+            </SidebarRightPreviewCardItem>
+        </SidebarRightPreviewCard>
     </div>
 </template>
 <script setup>
@@ -30,6 +47,25 @@ const whatsHappeningItems = ref([
     {
         title: '#artforall',
         count: '1.8k Tweets'
+    }
+])
+
+
+const whoToFollowItems = ref([
+    {
+        name: 'Joe Biden',
+        handle: '@JoeBiden',
+        image: 'https://picsum.photos/200/200'
+    },
+    {
+        name: 'Joe Biden',
+        handle: '@JoeBiden',
+        image: 'https://picsum.photos/200/200'
+    },
+    {
+        name: 'Joe Biden',
+        handle: '@JoeBiden',
+        image: 'https://picsum.photos/200/200'
     }
 ])
 
